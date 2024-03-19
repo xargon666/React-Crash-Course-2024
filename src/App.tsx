c35a1524238e1e33fe129d60eed8821495d862e7
Chris Pucknell <chris.pucknell@gmail.com>
2024-03-19 10:49:42 +0000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    const [x, y] = [10, 20];
+    const names = ["Brad", "Mary", "Joe", "Sarah"];
+    const loggedIn = true;
+    const styles = {
+color:'red', fontSize:24
+    }
+    return (
+        <>
+            <div className="text-5xl">App</div>
+            <p style={styles}>Hello {names[0]}</p>
+            <p>
+                The sum of {x} and {y} is {x + y}
+            </p>
+            <ul>
+                {names.map((name, index) => (
+                    <li key={index}>{name}</li>
+                ))}
+            </ul>
+            {loggedIn && <h1>Hello Member</h1>}
+        </>
+    );
+};
 
-export default App
+export default App;
