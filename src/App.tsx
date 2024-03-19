3ebe5ef4589756e5df0bf25ad2fa33d603296dad
Chris Pucknell <chris.pucknell@gmail.com>
2024-03-19 12:49:19 +0000

    setup Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
     return (
         <>
-            <Navbar/>
-            {/* <!-- Hero --> */}
-
-            <section className="bg-indigo-700 py-20 mb-4">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
-                    <div className="text-center">
-                        <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-                            Become a React Dev
-                        </h1>
-                        <p className="my-4 text-xl text-white">
-                            find the React job that fits your skills and needs
-                        </p>
-                    </div>
-                </div>
-            </section>
+            <Navbar />
+            <Hero title="Become a React Dev" subtitle="Find the React job that fits your skill set" />
 
             {/* <!-- Developers and Employers --> */}
             <section className="py-4">
