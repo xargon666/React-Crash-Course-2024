96c42f48627751d5b030e75635f6908da8ba39d7
Chris Pucknell <chris.pucknell@gmail.com>
2024-03-21 00:52:26 +0000

    add loading useState to JobListings

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -6,7 +6,10 @@ const JSON_SERVER_JOBS_URL = "http://localhost:5000/jobs";
 
 const JobListings = ({ isHome = false }) => {
     const [jobData, setJobData] = useState<JobProps[]>([]);
+    const [loading, setLoading] = useState(true);
+
     const jobListings = isHome ? jobData.slice(0, 3) : jobData;
+
     useEffect(() => {
         axios
             .get(JSON_SERVER_JOBS_URL)
@@ -16,26 +19,32 @@ const JobListings = ({ isHome = false }) => {
             .catch((err) => {
                 console.log(err);
             });
+        setLoading(false);
     }, []);
 
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    Browse Jobs
+                    {isHome?'Recent Jobs':'Browse Jobs'}
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobListings.map((job: JobProps) => (
-                        <Job
-                            key={job.id}
-                            id={job.id}
-                            type={job.type}
-                            title={job.title}
-                            description={job.description}
-                            salary={job.salary}
-                            location={job.location}
-                        />
+                    {loading ? (<h2>Loading...</h2>):
+                    <>
+                        {jobListings.map((job: JobProps) => (
+                            <Job
+                                key={job.id}
+                                id={job.id}
+                                type={job.type}
+                                title={job.title}
+                                description={job.description}
+                                salary={job.salary}
+                                location={job.location}
+                            
+                            />
                     ))}
+                    </>
+                    }
                 </div>
             </div>
         </section>
